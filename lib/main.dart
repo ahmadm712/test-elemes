a9a65f6a837a18425a1a8f942f7940d9dc3da12b
ahmadm712 <ahmadmuji2808@gmail.com>
2022-10-27 20:04:42 +0700

    change name

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Elemes Test',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
       onGenerateRoute: mainRoutes,
       home: const HomePage(),
